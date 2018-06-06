992157399cea77b52f0ff4a7ad85aaec24410b70
Mikej81 <stumpj@gmail.com>
2018-06-06 16:18:54 -0400

    authnrequest

--- extensions/saml_replay_ext/index.js
+++ extensions/saml_replay_ext/index.js
@@ -2,48 +2,280 @@
 var f5 = require('f5-nodejs');
 
 // Import the XML-Crypto Libraries
-// var saml = require('saml20');
 var zlib = require('zlib');
-var select = require('xml-crypto').xpath
-  , dom = require('xmldom').DOMParser
-  , SignedXml = require('xml-crypto').SignedXml
-  , FileKeyInfo = require('xml-crypto').FileKeyInfo  
-  , fs = require('fs');
-  
+var crypto = require('crypto');
+var xml2js = require('xml2js');
+var xmlCrypto = require('xml-crypto');
+var xmlbuilder = require('xmlbuilder');
+var xmlenc = require('xml-encryption');
+var select = require('xml-crypto').xpath;
+var dom = require('xmldom').DOMParser;
+var SignedXml = require('xml-crypto').SignedXml;
+var FileKeyInfo = require('xml-crypto').FileKeyInfo;
+var fs = require('fs');
+var querystring = require('querystring');
+var InMemoryCacheProvider = require('./inmemory-cache-provider.js').CacheProvider;
+var Q = require('q');
+
+//  Public Key of Issuer configured here  
 var signaturePubKey = "othercert.pem";
+
+function initialize(options) {
+  if (!options) {
+    options = {};
+  }
+
+  if (!options.path) {
+    options.path = '/saml/consume';
+  }
+  
+  if (!options.entryPoint) {
+      options.entryPoint = 'https://192.168.2.60/'
+  }
+
+  if (!options.host) {
+    options.host = 'localhost';
+  }
+  
+  if (!options.issuerURL) {
+      options.issuerURL = 'https://192.168.2.60/'
+  }
+
+  if (!options.issuer) {
+    options.issuer = 'f5-saml-replay';
+  }
+
+  if (options.identifierFormat === undefined) {
+    options.identifierFormat = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
+  }
+
+  if (options.authnContext === undefined) {
+    options.authnContext = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
+  }
+
+  if (!options.acceptedClockSkewMs) {
+    // default to no skew
+    options.acceptedClockSkewMs = 0;
+  }
+
+  if(!options.validateInResponseTo){
+    options.validateInResponseTo = false;
+  }
+
+  if(!options.requestIdExpirationPeriodMs){
+    options.requestIdExpirationPeriodMs = 28800000;  // 8 hours
+  }
+
+  if(!options.cacheProvider){
+      options.cacheProvider = new InMemoryCacheProvider(
+          {keyExpirationPeriodMs: options.requestIdExpirationPeriodMs });
+  }
+
+  if (!options.logoutUrl) {
+    // Default to Entry Point
+    options.logoutUrl = options.entryPoint || '';
+  }
+
+  // sha1, sha256, or sha512
+  if (!options.signatureAlgorithm) {
+    options.signatureAlgorithm = 'sha1';
+  }
+
+  return options;
+}
+
+function generateUniqueID() {
+  return crypto.randomBytes(10).toString('hex');
+}
+
+function generateInstant() {
+  return new Date().toISOString();
+}
+
+function signRequest(samlMessage) {
+  var signer;
+  var samlMessageToSign = {};
+  var options = initialize(options);
+  
+  switch(options.signatureAlgorithm) {
+    case 'sha256':
+      samlMessage.SigAlg = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256';
+      signer = crypto.createSign('RSA-SHA256');
+      break;
+    case 'sha512':
+      samlMessage.SigAlg = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha512';
+      signer = crypto.createSign('RSA-SHA512');
+      break;
+    default:
+      samlMessage.SigAlg = 'http://www.w3.org/2000/09/xmldsig#rsa-sha1';
+      signer = crypto.createSign('RSA-SHA1');
+      break;
+  }
+  if (samlMessage.SAMLRequest) {
+    samlMessageToSign.SAMLRequest = samlMessage.SAMLRequest;
+  }
+  if (samlMessage.SAMLResponse) {
+    samlMessageToSign.SAMLResponse = samlMessage.SAMLResponse;
+  }
+  if (samlMessage.RelayState) {
+    samlMessageToSign.RelayState = samlMessage.RelayState;
+  }
+  if (samlMessage.SigAlg) {
+    samlMessageToSign.SigAlg = samlMessage.SigAlg;
+  }
+  signer.update(querystring.stringify(samlMessageToSign));
+  samlMessage.Signature = signer.sign(options.privateCert, 'base64');
+}
+
+function requestToURL(request) {
+    var self = this;
+    var options = initialize(options);
+    var deflated = new Buffer(zlib.deflateRawSync(request), 'utf8');
+
+    var base64 = deflated.toString('base64');
+    var urlEnc = encodeURIComponent(base64.toString());
+
+    return urlEnc;
+}
+
+function getAdditionalParams(req, operation) {
+  var additionalParams = {};
+
+  var RelayState = req.query && req.query.RelayState || req.body && req.body.RelayState;
+  if (RelayState) {
+    additionalParams.RelayState = RelayState;
+  }
+
+  var optionsAdditionalParams = options.additionalParams || {};
+  Object.keys(optionsAdditionalParams).forEach(function(k) {
+    additionalParams[k] = optionsAdditionalParams[k];
+  });
+
+  var optionsAdditionalParamsForThisOperation = {};
+  if (operation == "authorize") {
+    optionsAdditionalParamsForThisOperation = options.additionalAuthorizeParams || {};
+  }
+  if (operation == "logout") {
+    optionsAdditionalParamsForThisOperation = options.additionalLogoutParams || {};
+  }
+
+  Object.keys(optionsAdditionalParamsForThisOperation).forEach(function(k) {
+    additionalParams[k] = optionsAdditionalParamsForThisOperation[k];
+  });
+
+  return additionalParams;
+}
 
 // Create a new rpc server for listening to TCL iRule calls.
 var ilx = new f5.ILXServer();
 
-ilx.addMethod('saml-validate', function(req, res) {
-    // Function parameters can be found in req.params().
-    //console.log('params: ' + req.params());
+ilx.addMethod('saml-request', function(req, res) {
+    var method = req.params()[0];
+    var landingURI = req.params()[1];
+    var sign = req.params()[2]
     
+    var isPassive = false;
+    
+    var self = this;
+    var options = initialize(options);
+    var id = "_" + generateUniqueID();
+    var instant = generateInstant();
+    var forceAuthn = options.forceAuthn || false;
+    
+    var request = {
+      'samlp:AuthnRequest': {
+        '@xmlns:samlp': 'urn:oasis:names:tc:SAML:2.0:protocol',
+        '@ID': id,
+        '@Version': '2.0',
+        '@IssueInstant': instant,
+        '@ProtocolBinding': 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+        '@AssertionConsumerServiceURL': 'https://' + landingURI,
+        '@Destination': options.entryPoint,
+        'saml:Issuer' : {
+          '@xmlns:saml' : 'urn:oasis:names:tc:SAML:2.0:assertion',
+          '#text': options.issuer
+        }
+      }
+    };
+
+    if (isPassive)
+      request['samlp:AuthnRequest']['@IsPassive'] = true;
+
+    if (forceAuthn) {
+      request['samlp:AuthnRequest']['@ForceAuthn'] = true;
+    }
+
+    if (options.identifierFormat) {
+      request['samlp:AuthnRequest']['samlp:NameIDPolicy'] = {
+        '@xmlns:samlp': 'urn:oasis:names:tc:SAML:2.0:protocol',
+        '@Format': options.identifierFormat,
+        '@AllowCreate': 'true'
+      };
+    }
+
+    if (!options.disableRequestedAuthnContext) {
+      request['samlp:AuthnRequest']['samlp:RequestedAuthnContext'] = {
+        '@xmlns:samlp': 'urn:oasis:names:tc:SAML:2.0:protocol',
+        '@Comparison': 'exact',
+        'saml:AuthnContextClassRef': {
+          '@xmlns:saml': 'urn:oasis:names:tc:SAML:2.0:assertion',
+          '#text': options.authnContext
+        }
+      };
+    }
+
+    if (options.attributeConsumingServiceIndex) {
+      request['samlp:AuthnRequest']['@AttributeConsumingServiceIndex'] = options.attributeConsumingServiceIndex;
+    }
+
+    if (options.providerName) {
+      request['samlp:AuthnRequest']['@ProviderName'] = options.providerName;
+    }
+
+    //callback(null, xmlbuilder.create(request).end());
+    //console.log(xmlbuilder.create(request).end());
+    //return xmlbuilder.create(request).end();
+    var authrequest;
+    //console.log("sign: " + sign);
+    if (sign == "true") {
+        authrequest = signRequest(xmlbuilder.create(request).end());
+    } else {
+        authrequest = xmlbuilder.create(request).end();        
+    }
+
+    var encodedAuthNRequest = requestToURL(authrequest);
+    
+    //console.log(encodedAuthNRequest);
+    
+    var redirectAuth = options.issuerURL + '?SAMLRequest=' + encodedAuthNRequest 
+    
+    res.reply(['OK', redirectAuth]);
+});
+
+ilx.addMethod('saml-validate', function(req, res) {
     var parse = req.params()[1];
 
     var URLDecodedAssertion = decodeURIComponent(req.params()[0]);
-    //console.log('URLdecoded: ' + URLDecodedAssertion);
-    
     var B64Assertion = new Buffer(URLDecodedAssertion, 'base64');
-    
     var rawAssertion = zlib.inflateRawSync(B64Assertion).toString('utf8');
-
     var doc = new dom().parseFromString(rawAssertion);
-    
     var signature = select(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
-
     var sig = new SignedXml();
     //IF keep this method, move outside of function, sync/async
     
-    sig.keyInfoProvider = new FileKeyInfo(signaturePubKey);
-    sig.loadSignature(signature);
-
-    var result = sig.checkSignature(rawAssertion);
     var isvalid;
-    if (!result) {
-        isvalid = sig.validationErrors;
+    if (typeof signature !== "undefined") {
+        sig.keyInfoProvider = new FileKeyInfo(signaturePubKey);
+        sig.loadSignature(signature);
+    
+        var result = sig.checkSignature(rawAssertion);
+        if (!result) {
+            isvalid = sig.validationErrors;
+        } else {
+            isvalid = result;
+        }
     } else {
-        isvalid = result;
+        isvalid = "Not Signed.";
     }
     
     var parsed;
@@ -61,18 +293,3 @@ ilx.addMethod('saml-validate', function(req, res) {
 
 // Start listening for ILX::call and ILX::notify events.
 ilx.listen();
-
-function myKeyInfo(x509Certificate){
-        this.getKeyInfo = function(key) {
-                return '<X509Data><X509Certificate>'+x509Certificate+'</X509Certificate></X509Data>';
-        };
-        this.getKey = function(keyInfo) {
-                // return the public key in pem format
-                return getPublicKeyPemFromCertificate(x509Certificate).toString();
-       };
-}
-
-
-
-
-
--- rules/saml_replay_tcl.tcl
+++ rules/saml_replay_tcl.tcl
@@ -7,82 +7,84 @@ when HTTP_REQUEST {
     set client_hash [sha512 $client]
     set tableName $client_hash
     set keyName "Token"
-    
-    set samlReplay_Handle [ILX::init saml_replay_plugin saml_replay_ext]
+    set apm_cookie [HTTP::cookie exists MRHSession]
 
+    set samlReplay_Handle [ILX::init saml_replay_plugin saml_replay_ext]
+    
+    if {$apm_cookie == 0} {
+    ## Start Switch METHOD
     switch [HTTP::method] {
         GET {
-            set encodedQuery "[URI::query [HTTP::uri] SAMLResponse]"
-            set decodedURI "[URI::decode [HTTP::uri] ]"
-            #set decodedQuery "[URI::query $decodedURI SAMLResponse]"
-      
-            #log local0. "encodedQ: $encodedQuery"
-            #log local0. "decodedQ: $decodedQuery"
-            #log local0. "decodedU: $decodedURI"
-            
-            set currentHost [HTTP::host]
-            
-            set samlResponse $encodedQuery
-            
-            if { ($samlResponse ne "") } {
-                log local0. "SAMLResponse exists, creating table entry"
-              if { ([table keys -subtable $tableName -count] == 0) } {
-                set tblcreate [table set -subtable $tableName $keyName $samlResponse 30]              
-                set saml_verify [ILX::call $samlReplay_Handle saml-validate $encodedQuery true]
-                log local0. "status: $saml_verify"
-                set signature_status [lindex $saml_verify 0]
-                set attributes [lindex $saml_verify 1]
-                set who_is_king [lindex $saml_verify 2]
-                table set -subtable $tableName verified $signature_status 30
-                if { ($signature_status == "1") } {
-                    HTTP::respond 302 Location "https://$currentHost&SAMLResponse=$samlResponse"
-                } elseif { $signature_status contains "Invalid" } {
-                # Invalid Signature
-                set html $signature_status
-                HTTP::respond 200 $html
-                }
-              } else {
-              # This whole else can be removed if there is a pool
-              # this is just for testing
-              if { ([table lookup -subtable $tableName verified] == "1" ) } {
-                  log local0. "verified, on redirect"
-                  HTTP::respond 200 "<HTML><BODY>YOU ARE GOOD!</BODY></HTML>"
-                  } else {
-                    log local0. "not verified"
-                  }
+            switch -glob [string tolower [URI::query [HTTP::uri]]] {
+                "*samlresponse*" { log local0. "samlresponse"
+                    set encodedQuery "[URI::query [HTTP::uri] SAMLResponse]"
+                    set decodedURI "[URI::decode [HTTP::uri] ]"
+                    set currentHost [HTTP::host]
+                    set samlResponse $encodedQuery
+
+                    if { ($samlResponse ne "") } {
+                        ## We make a table entry for the SAMLResponse so we can hold it
+                        ## till after the MRHSession is established,
+                        ## then we can replay it to the backend.  
+                        log local0. "SAMLResponse exists, creating table entry"
+                        if { ([table keys -subtable $tableName -count] == 0) } {
+                        set tblcreate [table set -subtable $tableName $keyName $samlResponse 30]
+                        set saml_verify [ILX::call $samlReplay_Handle saml-validate $encodedQuery true]
+                        log local0. "status: $saml_verify"
+                        set signature_status [lindex $saml_verify 0]
+                        set attributes [lindex $saml_verify 1]
+                        table set -subtable $tableName verified $signature_status 30
+                        if { ($signature_status contains "Invalid") } {
+                            # Invalid Signature
+                            set html $signature_status
+                            HTTP::respond 200 $html
+                        }
+                        if { ($signature_status contains "Not Signed") } {
+                            # SAMLResponse not signed, what do?
+                        }
               }
-            } else {
-                # Request was a GET, but no SAMLResponse so...
-                log local0. "No"
-                HTTP::respond 200 "No"
             }
+                }
+                default { 
+                ## If no SAMLRequest/SAMLResponse, MRHSession or POST:
+                ## Generate AuthNRequest
+                ## Config details for IDP and SP in index.js
+                ## Currently only coded for 302 right now...
+                    set callbackURI ""
+                    log local0. "No MRHSession Cookie, no Querystring, generate AuthNRequest"
+                    ## usage: saml-request [method] [callbackURI] [sign true/false]
+                    set AuthNRequest [ILX::call $samlReplay_Handle saml-request [HTTP::method] [HTTP::host][HTTP::uri] false]
+                    if { [lindex $AuthNRequest 0] eq "OK"} {
+                        HTTP::redirect [lindex $AuthNRequest 1]
+                    }
+                }
+            }
+
         }
         POST {
+            ## HTTP-POST BINDING
+            ## We only need to collect here, then process
+            ## under HTTP_REQUEST_DATA.
             log local0. "len = [HTTP::header Content-Length]"
             log local0. "req = [HTTP::request]"
-            # collect POST data
             HTTP::collect [HTTP::header Content-Length]
         }
     }
+    ##End Switch METHOD
+    #
+    }
 }
 when HTTP_REQUEST_DATA {
-    set samlReplay_Handle [ILX::init saml_replay_plugin saml_replay_ext]
+    set postReplay_Handle [ILX::init saml_replay_plugin saml_replay_ext]
 
     set POST_SAML_Response ""
     foreach x [split [string tolower [HTTP::payload]] "&"] {
         if {$x starts_with "samlresponse="} {
             set POST_SAML_Response [lindex [split $x "="] 1]
-            set saml_verify [ILX::call $samlReplay_Handle saml-validate $encodedQuery true]
+            set saml_verify [ILX::call $postReplay_Handle saml-validate $x true]
                 log local0. "status: $saml_verify"
         }
     }
-    
-
 }
-
-when CLIENT_CLOSED {
-    #table delete -subtable
-}
-
 
 
